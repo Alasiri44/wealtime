245e1af68470c050a49fcc1b8ae6a0ac73e550dc
alasiri <austinalasiri44@gmail.com>
2025-03-25 13:12:57 +0300

    Waiting for website to loead

--- src/script.js
+++ src/script.js
@@ -1,8 +1,10 @@
 
-require('dotenv').config();
+//require('dotenv').config();
 
-const openWather_apiKey = process.env.openWeather_apiKey;
+//const openWeather_apiKey = process.env.OPEN_WEATHER_API_KEY;
 
+
+const openWeather_apiKey = 'a028b132d6ba934de2daf58ff4577283';
 
 const displayedCity = document.getElementById('displayedCity')
 const weatherContainer = document.getElementById('weatherFound');
@@ -11,13 +13,18 @@ const weatherName = document.getElementById('weather');
 const weatherIcon = document.getElementById('icon');
 const imageCreated = document.getElementById('weather-image');
 const error = document.getElementById('error');
+const temperature = document.getElementById('current-temperature');
+const humidity = document.getElementById('current-humidity');
 
 function displayWeather(city='Nairobi'){   
     const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openWeather_apiKey}`  
     fetch(weatherApiUrl)
     .then(res => res.json())
     .then(data => {
+        console.log(data.main);
         data.weather.forEach(element => {
+            console.log(element);
+            
             displayedCity.textContent = city;
             weatherName.textContent = element.main;
 
@@ -26,7 +33,14 @@ function displayWeather(city='Nairobi'){
             weatherDescription.textContent = element.description; 
             error.classList.add('hidden');
             
-            })
+            })            
+                temperature.textContent = parseFloat((data.main.temp - 273.15).toFixed(2));
+                humidity.textContent = data.main.humidity;
+            
+    }).catch(error => {
+        console.log(error.message);
+        console.log('I am here');
+        
     })
 
 }
@@ -42,9 +56,23 @@ function allowingSearching(){
     })
 }
 
+function currentCity(){
+    function successCallback(position){
+        console.log(position);        
+    }
+    function errorCallback(position){
+        console.log(position);        
+    }
+
+    const watchId =navigator.geolocation.watchPosition(successCallback, errorCallback);
+    console.log(watchId);
+}   
+
+
 function main(){
     displayWeather();
     allowingSearching();
+   // currentCity();
 }
 
 document.addEventListener('DOMContentLoaded', function(){
